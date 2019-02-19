6921610cadc987a0e55ffe84633fd79b07651d10
Stephen Geller <stephen.gb.geller+github@gmail.com>
2019-02-19 15:39:30 +0000

    feat(dojo-template): Improve deploy script output and behaviour.
    * Variables are now script arguments

--- deploy.sh
+++ deploy.sh
@@ -1,10 +1,22 @@
 #!/usr/bin/env bash
 
+RED="\033[31m"
+GREEN="\033[32m"
+BOLD="\033[1m"
+NC="\033[0m"
+
 PACKAGE_DIR=package # Directory to bundle everything up
-FUNCTION_NAME=SOME_LAMBDA_FUNCTION_NAME # Change this
+FUNCTION_NAME=$1 # Change this
 ZIPPED_PACKAGE=${FUNCTION_NAME}.zip # So that we can use it later
-REGION=SOME_REGION # Your chosen AWS region, eg eu-west-1
-PROFILE=SOME_PROFILE # Write "default" if you don't have more fancy configuration
+REGION=$2 # Your chosen AWS region, eg eu-west-1
+PROFILE="${3:-default}" # Use "default" if you don't have fancy configuration in your ~/.aws/credentials
+
+if [[ -z ${FUNCTION_NAME} ||  -z ${REGION} ||  -z ${PROFILE} ]]; then
+    echo -e "${RED}Error! Missing config in deploy.sh"
+    echo -e "Make sure you pass FUNCTION_NAME, REGION and PROFILE variables."
+    echo -e "${BOLD}e.g. ${0} some_function eu-west-1 some_user1${NC}"
+    exit 1
+fi
 
 
 # Install dependencies from requirements.txt
@@ -17,17 +29,22 @@ function install_dependencies() {
 function zip_files() {
     cp src/* ${PACKAGE_DIR}/
     cd ${PACKAGE_DIR}
-    echo "Zipping up..."
+    echo -e "\n${BOLD}Zipping up...${NC}"
     zip -r ../${ZIPPED_PACKAGE} *
     cd - &>/dev/null
 }
 
 # Upload the zipped directory straight to the AWS Lambda,
 function upload_to_aws() {
-    echo "Uploading to AWS Lambda..."
+    echo -e "\n${BOLD}Uploading to AWS Lambda...${NC}"
     aws lambda update-function-code --profile ${PROFILE} --publish --region ${REGION} --function-name ${FUNCTION_NAME} --zip-file fileb://${ZIPPED_PACKAGE}
+    EXIT_CODE=$?
+    if [[ ${EXIT_CODE} -ne 0 ]]; then
+        echo -e "\n***${RED} Failed to upload to AWS Lambda ${NC}***"
+    fi
+
     cleanup
-    echo "Done."
+    echo -e "${GREEN}Done. ${NC}"
 }
 
 # Remove generated directory and zipped package
